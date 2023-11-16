9dac307b56a4ec89031e705d0efa7f6fecd45424
prabhatamit3 <42322211+prabhatamit3@users.noreply.github.com>
2023-11-16 18:41:23 +0530

    latest changes

---
+++ config/mongoose.js
@@ -0,0 +1,22 @@
+// requiring the library
+const mongoose = require("mongoose");
+
+//connecting to the database
+mongoose.connect(
+  'mongodb+srv://prabhatamit2:prabhatamit2@cluster0.1dwmaoa.mongodb.net/Schemas?retryWrites=true&w=majority'
+  
+);
+
+// acquiring the connection to check if it is succesfull
+const db = mongoose.connection;
+
+// checking for the error
+db.on("error", console.error.bind(console, "error in connecting the database"));
+
+// up and running then print the statement
+db.once("open", () => {
+  console.log("succesfully connected to database");
+});
+
+// exporting the connection
+module.exports = db;
